4f8412c47b50e5e09fdf1c40dcbf31447e6221a3
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-10-08 17:55:54 -0500

    Add files via upload

--- Monkey Business/client/components/App.jsx
+++ Monkey Business/client/components/App.jsx
@@ -1,61 +1,22 @@
 import React from 'react'
-
-import PageHeader from './PageHeader.jsx'
-//import StockTable from './PortfolioPage.jsx'
-import StockDetails from './StockDetails.jsx'
-import BootstrapModal from './BootstrapModal.jsx'
-import FormModal from './FormModal.jsx'
-//import LoginRegisterForm from './LoginRegisterForm.jsx'
-import { retrieveStockDetails } from '../mbdataHelper'
-
-export default function App (props) {
-
-  const [currentStock, setCurrentStockID] = React.useState('')
-  const [showDetailsModal, setShowDetailsModal] = React.useState(false)
-  const [showFormModal, setShowFormModal] = React.useState(false)
-
-  const detailsRequested = (stockID) => {
-    console.log('details requested for stockID: ' + stockID)
-    setCurrentStockID(stockID)
-    setShowDetailsModal(true)
-  }
-  const [currentStockData, setCurrentStockData] = React.useState(null)
-  React.useEffect(() => {
-    const fetchStockData = async () => {
-      const stockData = await retrieveStockDetails(currentStock)
-      setCurrentStockData(stockData)
-    }
-
-    if (currentStock !== '') {
-    fetchStockData(currentStock)
-    }
-  } , [currentStock])
+import { Route, Routes } from 'react-router-dom'
+import MyNavBar from './Navigation/MyNavBar.jsx'
+import Home from './Home/HomePage.jsx'
+import LoginPage from './Login_Signup/LoginPage.jsx'
+import SignupPage from './Login_Signup/SignupPage.jsx'
+import MonkeTech from './MonkeyTech/MonkeyTechPage.jsx'
+import IntroPage from './Intro/IntroPage.jsx'
+export default function App () {
   return (
-    // <div className='container'>
-    //   <PageHeader title='Stock Browser' subTitle='' />
-    //   <StockGrid onDetailsRequested={detailsRequested}/>
-    //   <BootstrapModal
-    //   open={showDetailsModal}
-    //   stockName={`${currentStockData?.name} (${currentStockData?.stockID})`}
-    //   onClose={() => setShowDetailsModal(false)}
-    //   >
-
-    //     {!!currentStockData && <StockDetails {...currentStockData} />}
-    //   </BootstrapModal>
-    //   <FormModal
-    //   open={showFormModal}
-    //   stockName={`${currentStockData?.name} (${currentStockData?.stockID})`}
-    //   onClose={() => setShowFormModal(false)}
-    //   >
-
-    //     {!!currentStockData && <StockForm {...currentStockData} />}
-    //   </FormModal>
-
-
-    // </div>
-    <div className='container'>
-      <p>Hello World!</p>\
-    </div>
-
+    <React.Fragment>
+    <MyNavBar/>
+    <Routes>
+        <Route path="/" exact Component={Home } />
+        <Route path="/login" Component={LoginPage } />
+        <Route path="/about" Component={IntroPage } />
+        <Route path = "/signup" Component={SignupPage}/>
+        <Route path = "/monkeyTech" Component = {MonkeTech}/>
+    </Routes>
+    </React.Fragment>
   )
 }
--- Monkey Business/client/components/PageHeader.jsx
+++ Monkey Business/client/components/PageHeader.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'react-bootstrap'
 
 export default function PageHeader (props) {
-  const { title, subTitle } = props
+  const { title } = props
 
   return (
-    <div className="pb-2 mt-4 mb-2 border-bottom" style={{ width: '100%' }}>
-      <h1>{title}</h1>
-      {subTitle}
-    </div>
+    // <div className="pb-2 mt-4 mb-2 border-bottom" style={{ width: '100%' }}>
+    //   <h1>{title}</h1>
+    //   {subTitle}
+    // </div>
+    <header>
+      <div>
+        {title}
+      </div>
+      <Button style={{ float: 'right' }}> Log In </Button>
+      <Button style={{ float: 'right' }}> Sign Up </Button>
+    </header>
   )
 }
 
 PageHeader.propTypes = {
-  title: PropTypes.string,
-  subTitle: PropTypes.string
+  title: PropTypes.string
 }
 
 PageHeader.defaultProps = {
-  title: 'Page Title',
-  subTitle: 'Page Subtitle'
+  title: 'Page Title'
 }
