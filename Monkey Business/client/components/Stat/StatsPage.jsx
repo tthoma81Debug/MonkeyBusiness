8ef2f08474ce359a6ca5d579284fc25848770558
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-10-08 21:38:18 -0500

    Add files via upload

---
+++ Monkey Business/client/components/Stat/StatsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function StatsPage() {
+  return (
+    <div>
+      <div>
+        <h1>Stats Page</h1>
+      </div>
+
+      <div>
+        <p>This is where Stats stuff will go</p>
+      </div>
+    </div>
+  )
+}
+
+export default StatsPage;
