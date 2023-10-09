482c7b883b55428ba979685d30baa1b1f70df085
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-10-08 21:39:10 -0500

    Add files via upload

--- Monkey Business/client/components/App.jsx
+++ Monkey Business/client/components/App.jsx
@@ -6,6 +6,8 @@ import LoginPage from './Login_Signup/LoginPage.jsx'
 import SignupPage from './Login_Signup/SignupPage.jsx'
 import MonkeTech from './MonkeyTech/MonkeyTechPage.jsx'
 import IntroPage from './Intro/IntroPage.jsx'
+import SettingsPage from './Setting/SettingsPage.jsx'
+import StatsPage from './Stat/StatsPage.jsx'
 export default function App () {
   return (
     <React.Fragment>
@@ -16,6 +18,8 @@ export default function App () {
         <Route path="/about" Component={IntroPage } />
         <Route path = "/signup" Component={SignupPage}/>
         <Route path = "/monkeyTech" Component = {MonkeTech}/>
+        <Route path = "/setting" Component = {SettingsPage}/>
+        <Route path = "/stats" Component = {StatsPage}/>
     </Routes>
     </React.Fragment>
   )
